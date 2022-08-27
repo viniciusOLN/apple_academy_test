b7d5ea4106a0dcfabc13df31782b2a09f08917e9
vinicius <vinicius853o@gmail.com>
2022-08-27 19:04:29 -0300

    calling the function and showing the result

--- main.dart
+++ main.dart
@@ -1,92 +1,15 @@
+import 'dataStudents.dart';
+import 'getGroups.dart';
+
 void main() {
-  Map<String, dynamic> dataStudents = {
-    "ciclo1": [
-      ["Laura", "Pedro", "João", "Vinicius"],
-      ["Carlos", "Maria", "Leonardo", "Ana"],
-      ["Daniela", "Marcos", "Wesley", "Luiza"],
-      ["Daiane", "Felipe", "Teodoro", "Helena"],
-      ["Natalia", "Beatriz", "Eduardo", "Caio"]
-    ],
-    "ciclo2": [
-      ["Teodoro", "Daiane", "Luiza"],
-      ["Carlos", "João", "Helena"],
-      ["Daniela", "Pedro", "Caio"],
-      ["Leonardo", "Maria", "Laura"],
-      ["Beatriz", "Marcos", "Vinicius"],
-      ["Natalia", "Felipe", "Eduardo"],
-      ["Ana", "Wesley"]
-    ]
-  };
+  int studentsPerGroup = 2;
 
-  List<List<String>> resultTeam = [];
+  List<List<String>> groups = getGroups(
+    studentsPerGroup,
+    students,
+    dataStudents,
+  );
 
-  bool isInTheSameTeam(String firstStudent, String secondStudent) {
-    bool isIntheSameTeam = false;
-    for (int i = 1; i < dataStudents.length; i++) {
-      dataStudents['ciclo${i}'].forEach((e) {
-        if (e.contains(firstStudent) && e.contains(secondStudent)) {
-          isIntheSameTeam = true;
-        }
-      });
-    }
-    return isIntheSameTeam;
-  }
-
-  int currentStudent = 0;
-  int nextStudent = 1;
-  List<String> students = [
-    "Laura",
-    "Pedro",
-    "João",
-    "Vinicius",
-    "Carlos",
-    "Maria",
-    "Leonardo",
-    "Ana",
-    "Daniela",
-    "Marcos",
-    "Wesley",
-    "Luiza",
-    "Daiane",
-    "Felipe",
-    "Teodoro",
-    "Helena",
-    "Natalia",
-    "Beatriz",
-    "Eduardo",
-    "Caio",
-  ];
-
-  int quantStudentsPerTeam = 3;
-  List<String> sameGroup = [];
-  while (nextStudent < students.length) {
-    String current = students[currentStudent];
-    String next = students[nextStudent];
-
-    if (sameGroup.length == 0) {
-      sameGroup.add(current);
-    }
-
-    if (sameGroup.any((e) => isInTheSameTeam(e, next)) ||
-        sameGroup.contains(current) && sameGroup.contains(next)) {
-      nextStudent++;
-      continue;
-    }
-
-    sameGroup.add(next);
-
-    if (sameGroup.length == quantStudentsPerTeam) {
-      students = students.where((student) {
-        return !sameGroup.contains(student);
-      }).toList();
-
-      resultTeam.add([...sameGroup]);
-      sameGroup.clear();
-      currentStudent = 0;
-      nextStudent = currentStudent + 1;
-    }
-  }
-
-  print(resultTeam);
-  print(students.length);
+  print("Equipes: " + groups.toString());
+  print("Alunos que sobraram: " + students.toString());
 }
