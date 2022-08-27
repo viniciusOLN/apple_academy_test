471b34bb273b0ea6f0f44d8fdc140897e83883be
vinicius <vinicius853o@gmail.com>
2022-08-27 19:05:45 -0300

    function responsable for get all groups formated

---
+++ getGroups.dart
@@ -0,0 +1,59 @@
+import 'isInTheSameTeam.dart';
+
+List<List<String>> _getTeams(int limit, List<String> students, Map data) {
+  List<String> sameGroup = [];
+  List<List<String>> resultTeam = [];
+  int currentStudent = 0;
+  int nextStudent = 1;
+
+  while (nextStudent < students.length) {
+    String current = students[currentStudent];
+    String next = students[nextStudent];
+
+    if (sameGroup.length == 0) {
+      sameGroup.add(current);
+    }
+
+    bool isAllInSameTeam = sameGroup.any((e) => isInTheSameTeam(e, next, data));
+    bool bothInGroup = sameGroup.contains(current) && sameGroup.contains(next);
+
+    if (isAllInSameTeam || bothInGroup) {
+      nextStudent++;
+      continue;
+    }
+
+    sameGroup.add(next);
+
+    if (sameGroup.length == limit) {
+      students = students.where((student) {
+        return !sameGroup.contains(student);
+      }).toList();
+
+      resultTeam.add([...sameGroup]);
+      sameGroup.clear();
+      currentStudent = 0;
+      nextStudent = currentStudent + 1;
+    }
+  }
+  return resultTeam;
+}
+
+List<List<String>> getGroups(int limit, List<String> students, Map data) {
+  List<List<String>> finalGroups = [];
+
+  for (int i = limit; i != 1; i--) {
+    List<List<String>> groupsFormated = _getTeams(i, students, data);
+
+    groupsFormated.forEach(
+      (row) => row.forEach(
+        (studentAtRow) => students.removeWhere(
+          (student) => student == studentAtRow,
+        ),
+      ),
+    );
+
+    groupsFormated.forEach((row) => finalGroups.add(row));
+  }
+
+  return finalGroups;
+}
